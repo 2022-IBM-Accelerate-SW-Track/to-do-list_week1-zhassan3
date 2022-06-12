2bdce0b0e76383b7fe945a8be6b63a2f6f72c50d
zhassan3 <zhassan3@mit.edu>
2022-06-12 16:25:02 -0400

    added all the changes from local repo

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,10 +1,34 @@
 import React, { Component } from 'react';
-  
+import imag from "../assets/zhheadshot.jpg";
+import "./About.css";
 export default class About extends Component {
   render() {
     return (
+
       <div>
-        <p>Design your About me page </p> 
+        <div class="split left">
+          <div className="centered">
+            <img
+              class="profile_image"
+
+              alt="Profile Pic"
+              src={imag}
+
+            />
+          </div>
+        </div>
+        <div className="split right">
+          <div className="centered">
+            <div className="name_title">Ziyad Hassan</div>
+            <div className="brief_description">
+              <text>
+                Hello! My name is Ziyad Hassan and I am incoming sophmore studying computer science @ MIT. I run cross country and track for my school,but I also do it for fun
+                Asides from that I love taking walks along scenic places and taking pictures. I love getting to know people and making genuine connections, so feel free to hit me up!
+              </text>
+            </div>
+          </div>
+        </div>
+
       </div>
     )
   }
